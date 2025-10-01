9f2b6b7f455701611b03d5790c6d3f22bbb52fbd
Jinadev <jinad3v@gmail.com>
2025-10-01 14:08:44 +0530

    overflow hidden and heading for file added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./Components/footer";
 function App() {
   return (
     // main div of BEAUFORTE
-    <div className="">
+    <div className="overflow-x-hidden">
       <Navbar />
       <Hero />
       <Explore/>
